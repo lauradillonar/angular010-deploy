d30d6a7683793be4bbc72575a83612aa644defd8
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-12 22:14:14 -0300

    Prueba de deploy a GitHub Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular010-deploy';
+  title = 'Hola mundo - Deploy a GitHub Pages';
 }
